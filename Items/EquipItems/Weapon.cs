1e240e5cd6bd247a16126ceaf0a06bcb8a88f48f
chris8jarvis <christinaeduardovna8@gmail.com>
2026-01-15 16:34:15 +0300

    grindstone usage and weapon durab init and usage items fixed room direction fixed

--- Items/EquipItems/Weapon.cs
+++ Items/EquipItems/Weapon.cs
@@ -4,7 +4,7 @@ namespace GamePrototype.Items.EquipItems
 {
     public sealed class Weapon : EquipItem
     {
-        public Weapon(uint damage, uint durability, string name) : base(durability, name) => Damage = damage;
+        public Weapon(uint damage, uint durability, string name) : base(durability, name) => Damage = damage; // добавить присвоение прочности
 
         public uint Damage { get; }
 
