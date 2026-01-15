f21004bcc5112af43f2209c1f9e2dd8de3f73d61
chris8jarvis <christinaeduardovna8@gmail.com>
2026-01-15 12:40:28 +0300

    added armour reduce

--- Items/EquipItems/EquipItem.cs
+++ Items/EquipItems/EquipItem.cs
@@ -12,9 +12,23 @@ namespace GamePrototype.Items.EquipItems
 
         public abstract EquipSlot Slot { get; }
 
-        protected EquipItem(uint maxDurability, string name) : base(name) => _maxDurability = maxDurability;
+        protected EquipItem(uint maxDurability, string name) : base(name)
+        {
+            _maxDurability = maxDurability;
+            _durability = _maxDurability;
+        }
 
-        public void ReduceDurability(uint delta) => _durability -= delta;
+        public void ReduceDurability(uint durabilityReducingDelta)
+        {
+            if (_durability <= durabilityReducingDelta)
+            {
+                _durability = 0;
+            }
+            else
+            {
+                _durability -= durabilityReducingDelta;
+            }
+        }
 
         public void Repair(uint delta) => 
             _durability += _durability + delta > _maxDurability 
